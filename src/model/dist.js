5bb54250ab7262667adf34e281e7f1b31f1b66e2
PiIsFour <PiIsFour@users.noreply.github.com>
2018-03-16 06:56:53 +0100

    add calc for cred interval of distribution

--- src/model/dist.js
+++ src/model/dist.js
@@ -17,3 +17,30 @@ export function normalize (dist) {
 	const sum = dist.reduce((sum, curr) => sum + curr.prob, 0)
 	return dist.map(o => ({...o, prob: o.prob / sum}))
 }
+
+export function interval (dist, minProb) {
+	function * generator (dist, minProb) {
+		let array = dist.slice(0)
+		let cut = 0
+		while (cut + minProb <= 1) {
+			yield array
+			cut += array[0].prob
+			array = array.slice(1)
+		}
+	}
+	return [...generator(dist, minProb)]
+		.map(dist => dist.reduce(
+			(interval, curr) => {
+				if (interval.prob >= minProb) {
+					return interval
+				} else {
+					return {
+						...interval,
+						end: curr.param,
+						prob: interval.prob + curr.prob
+					}
+				}
+			}, {start: dist[0].param, prob: 0}))
+		.sort((first, second) => first.prob < second.prob)
+		.sort((first, second) => first.end - first.start > second.end - second.start)[0]
+}
--- src/model/dist.test.js
+++ src/model/dist.test.js
@@ -1,4 +1,4 @@
-import {create, normalize} from './dist'
+import {create, normalize, interval} from './dist'
 
 describe('dist', () => {
 	it('creates an even distribution', () => {
@@ -18,5 +18,21 @@ describe('dist', () => {
 			{param: 0.3, prob: 0.5},
 			{param: 1, prob: 0.25}
 		])
+	})
+
+	it('calculates the smallest intervall with given prob', () => {
+		expect(interval([
+			{param: 0, prob: 0.25},
+			{param: 0.3, prob: 0.5},
+			{param: 1, prob: 0.25}
+		], 0.7)).toEqual({start: 0, end: 0.3, prob: 0.75})
+	})
+
+	it('intervall favors heigher prob when same lenght', () => {
+		expect(interval([
+			{param: 0, prob: 0.2},
+			{param: 0.5, prob: 0.5},
+			{param: 1, prob: 0.3}
+		], 0.6)).toEqual({start: 0.5, end: 1, prob: 0.8})
 	})
 })
